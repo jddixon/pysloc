9a978258126554b5158499e3359e4908eeb8afcf
Jim Dixon <jddixon@gmail.com>
2016-05-01 08:15:04 -0700

    add .dvcz/projCfg*

--- pysloc/__init__.py
+++ pysloc/__init__.py
@@ -45,8 +45,8 @@ __all__ = ['__version__', '__version_date__',
            ]
 
 # exported constants ------------------------------------------------
-__version__      = '0.8.6'
-__version_date__ = '2016-04-20'
+__version__ = '0.8.7'
+__version_date__ = '2016-05-01'
 
 # private constants -------------------------------------------------
 GPERF_RE = re.compile(
@@ -1803,4 +1803,3 @@ def countLinesXml(pathToFile, options, lang):
     except Exception as e:
         print("error parsing '%s', skipping: %s" % (pathToFile, e))
     return lineCount, slocSoFar
-
