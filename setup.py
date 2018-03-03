9ac9d6c1337b85a694be492b486948bf906cb622
Jim Dixon <jddixon@gmail.com>
2018-03-03 13:01:44 -0800

    fix setup.py, tox config files

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if exists('README.md'):
         LONG_DESC = file.read()
 
 setup(name='pysloc',
-      version='0.9.6',
+      version='0.9.7',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
       long_description=LONG_DESC,
@@ -31,8 +31,6 @@ setup(name='pysloc',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
           'Programming Language :: Python 2.7',
-          'Programming Language :: Python 3.3',
-          'Programming Language :: Python 3.4',
           'Programming Language :: Python 3.5',
           'Programming Language :: Python 3.6',
           'Programming Language :: Python 3.7',
